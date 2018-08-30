e86c8c6f4926b4a0b1826ad0217abb5faef06d1e
blueVegetable <2601042086@qq.com>
2018-08-30 22:23:00 +0800

    文章功能初步完成

--- target/studio1_view1/ManagerSystem/js/function.js
+++ target/studio1_view1/ManagerSystem/js/function.js
@@ -68,3 +68,14 @@ function getType(type) {
         default : return "";
     }
 }
+function getParameter(href,key) {
+    var parameterString=href.split("?")[1];
+    if(parameterString==null)
+        return "";
+    var parameters=parameterString.split("&");
+    for(var i=parameters.length-1;i>=0;i--) {
+        if(parameters[i].split("=")[0]==key)
+            return parameters[i].split("=")[1];
+    }
+    return "";
+}
