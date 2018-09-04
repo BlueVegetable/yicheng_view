d5ad9486bf61cdd0b9ce097f0a8e577c15650d44
blueVegetable <2601042086@qq.com>
2018-09-04 12:49:33 +0800

    将添加文章功能剔除掉了

--- src/main/webapp/ManagerSystem/js/url.js
+++ src/main/webapp/ManagerSystem/js/url.js
@@ -1,5 +1,6 @@
 // var remoteURL="http://localhost:8080/studio1";
-// var remoteURL="http://114.67.130.29/yicheng";
+var remoteURL="http://114.67.130.29/yicheng";
 // var remoteURL="http://localhost:80/yicheng";
-var remoteURL="http://localhost:80/studio1";
-var remoteImageURL = "http://localhost:80/upload";
+// var remoteURL="http://localhost:80/studio1";
+// var remoteImageURL = "http://localhost:80/upload";
+var remoteImageURL = "http://114.67.130.29/upload";
