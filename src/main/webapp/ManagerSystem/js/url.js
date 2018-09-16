6036f182cc14077dec4f25fb9e50f00d888c2a33
blueVegetable <2601042086@qq.com>
2018-09-16 10:09:31 +0800

    添加了新的报名表

--- src/main/webapp/ManagerSystem/js/url.js
+++ src/main/webapp/ManagerSystem/js/url.js
@@ -1,6 +1,8 @@
+var remoteURL="http://114.67.130.29/yicheng";
+var remoteImageURL = "http://114.67.130.29/upload";
+
+// var remoteURL="http://localhost:80/studio1";
+// var remoteImageURL = "http://localhost:80/upload";
+
 // var remoteURL="http://localhost:8080/studio1";
-// var remoteURL="http://114.67.130.29/yicheng";
 // var remoteURL="http://localhost:80/yicheng";
-var remoteURL="http://localhost:80/studio1";
-var remoteImageURL = "http://localhost:80/upload";
-// var remoteImageURL = "http://114.67.130.29/upload";
