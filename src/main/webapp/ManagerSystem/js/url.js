84f8f9105607d217da0a074445b4b86eb5f9f941
blueVegetable <2601042086@qq.com>
2018-09-26 23:52:30 +0800

    增加了管理

--- src/main/webapp/ManagerSystem/js/url.js
+++ src/main/webapp/ManagerSystem/js/url.js
@@ -1,8 +1,8 @@
-var remoteURL="http://114.67.130.29/yicheng";
-var remoteImageURL = "http://114.67.130.29/upload";
+// var remoteURL="http://114.67.130.29/yicheng";
+// var remoteImageURL = "http://114.67.130.29/upload";
 
-// var remoteURL="http://localhost:80/studio1";
-// var remoteImageURL = "http://localhost:80/upload";
+var remoteURL="http://localhost:80/studio1";
+var remoteImageURL = "http://localhost:80/upload";
 
 // var remoteURL="http://localhost:8080/studio1";
 // var remoteURL="http://localhost:80/yicheng";
