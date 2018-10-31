51b1d0c3d8dc7d98b20b7b986d239b8e84f68d2c
blueVegetable <2601042086@qq.com>
2018-10-31 19:26:41 +0800

    增加了咨询表单中的删除按钮

--- target/studio1_view1/ManagerSystem/js/url.js
+++ target/studio1_view1/ManagerSystem/js/url.js
@@ -1,8 +1,11 @@
 // var remoteURL="http://114.67.130.29/yicheng";
 // var remoteImageURL = "http://114.67.130.29/upload";
 
-var remoteURL="http://localhost:80/studio1";
-var remoteImageURL = "http://localhost:80/upload";
+// var remoteURL="http://localhost:80/studio1";
+// var remoteImageURL = "http://localhost:80/upload";
 
 // var remoteURL="http://localhost:8080/studio1";
 // var remoteURL="http://localhost:80/yicheng";
+
+var remoteURL="http://localhost:80/yicheng";
+var remoteImageURL = "http://localhost:80/upload";
