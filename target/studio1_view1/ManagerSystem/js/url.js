672918970ecec860506f1b4c99d2dae85c6d2ef9
blueVegetable <2601042086@qq.com>
2018-11-10 17:23:02 +0800

    轮播图pc端，文章功能

--- target/studio1_view1/ManagerSystem/js/url.js
+++ target/studio1_view1/ManagerSystem/js/url.js
@@ -1,3 +1,4 @@
+//上线服务器
 // var remoteURL="http://114.67.130.29/yicheng";
 // var remoteImageURL = "http://114.67.130.29/upload";
 
@@ -7,8 +8,10 @@
 // var remoteURL="http://localhost:8080/studio1";
 // var remoteURL="http://localhost:80/yicheng";
 
+//本地服务器
 // var remoteURL="http://localhost:80/yicheng";
 // var remoteImageURL = "http://localhost:80/upload";
 
+//局域网服务器
 var remoteURL = "http://192.168.1.101:8081/yicheng";
 var remoteImageURL = "http://192.168.1.101:8081/upload";
