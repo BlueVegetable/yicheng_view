fe27df15dee10bc898ceff2985e110f636baa8c7
BlueVegetable <2601042086@qq.com>
2018-11-27 19:55:32 +0800

    完成了首页文章类型模块图片的功能

--- src/main/webapp/ManagerSystem/js/url.js
+++ src/main/webapp/ManagerSystem/js/url.js
@@ -9,9 +9,13 @@
 // var remoteURL="http://localhost:80/yicheng";
 
 //本地服务器
-var remoteURL="http://localhost:80/yicheng";
-var remoteImageURL = "http://localhost:80/upload";
+// var remoteURL="http://localhost:80/yicheng";
+// var remoteImageURL = "http://localhost:80/upload";
 
 //局域网服务器
 // var remoteURL = "http://192.168.1.101:8081/yicheng";
 // var remoteImageURL = "http://192.168.1.101:8081/upload";
+
+//阿里云服务器
+var remoteURL="http://119.23.17.111:8083/yicheng";
+var remoteImageURL = "http://119.23.17.111:8083/upload";
