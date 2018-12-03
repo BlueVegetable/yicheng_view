2d6b7b66dbf93bb857d6d83b2832a9241aba6afd
BlueVegetable <2601042086@qq.com>
2018-12-03 21:56:03 +0800

    完成了学生作业的功能

--- src/main/webapp/ManagerSystem/js/url.js
+++ src/main/webapp/ManagerSystem/js/url.js
@@ -1,6 +1,6 @@
 //上线服务器
-var remoteURL="http://114.67.130.29/yicheng";
-var remoteImageURL = "http://114.67.130.29/upload";
+// var remoteURL="http://114.67.130.29/yicheng";
+// var remoteImageURL = "http://114.67.130.29/upload";
 
 // var remoteURL="http://localhost:80/studio1";
 // var remoteImageURL = "http://localhost:80/upload";
@@ -9,8 +9,8 @@ var remoteImageURL = "http://114.67.130.29/upload";
 // var remoteURL="http://localhost:80/yicheng";
 
 //本地服务器
-// var remoteURL="http://localhost:80/yicheng";
-// var remoteImageURL = "http://localhost:80/upload";
+var remoteURL="http://localhost:80/yicheng";
+var remoteImageURL = "http://localhost:80/upload";
 
 //局域网服务器
 // var remoteURL = "http://192.168.1.101:8081/yicheng";
