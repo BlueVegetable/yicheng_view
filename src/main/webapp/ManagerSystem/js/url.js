89b1d367ac569d00512b91efa947950febb28f03
BlueVegetable <2601042086@qq.com>
2018-12-16 17:19:10 +0800

    更换ip地址

--- src/main/webapp/ManagerSystem/js/url.js
+++ src/main/webapp/ManagerSystem/js/url.js
@@ -1,6 +1,10 @@
 //上线服务器
-var remoteURL="http://114.67.130.29/yicheng";
-var remoteImageURL = "http://114.67.130.29/upload";
+// var remoteURL="http://114.67.130.29/yicheng";
+// var remoteImageURL = "http://114.67.130.29/upload";
+
+//上线服务器：替换
+var remoteURL="http://106.13.32.172:8081/yicheng";
+var remoteImageURL = "http://106.13.32.172:8081/upload";
 
 // var remoteURL="http://localhost:80/studio1";
 // var remoteImageURL = "http://localhost:80/upload";
