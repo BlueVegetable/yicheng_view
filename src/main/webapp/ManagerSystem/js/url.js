67a76493732badc0f3daf801bb63cecc819a3223
BlueVegetable <2601042086@qq.com>
2018-12-31 01:28:53 +0800

    完成了首页轮播图的后台管理界面

--- src/main/webapp/ManagerSystem/js/url.js
+++ src/main/webapp/ManagerSystem/js/url.js
@@ -3,8 +3,8 @@
 // var remoteImageURL = "http://114.67.130.29/upload";
 
 //上线服务器：替换
-var remoteURL="http://106.13.32.172:8081/yicheng";
-var remoteImageURL = "http://106.13.32.172:8081/upload";
+// var remoteURL="http://106.13.32.172:8081/yicheng";
+// var remoteImageURL = "http://106.13.32.172:8081/upload";
 
 // var remoteURL="http://localhost:80/studio1";
 // var remoteImageURL = "http://localhost:80/upload";
@@ -13,8 +13,8 @@ var remoteImageURL = "http://106.13.32.172:8081/upload";
 // var remoteURL="http://localhost:80/yicheng";
 
 //本地服务器
-// var remoteURL="http://localhost:80/yicheng";
-// var remoteImageURL = "http://localhost:80/upload";
+var remoteURL="http://localhost:80/yicheng";
+var remoteImageURL = "http://localhost:80/upload";
 
 //局域网服务器
 // var remoteURL = "http://192.168.1.101:8081/yicheng";
